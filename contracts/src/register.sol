d595aaa7868562afc25d1cf7d5f5a1458219e97e
MBelegris <mikeybel98@gmail.com>
2024-03-05 11:47:34 +0000

    empty voting pair added to user map

--- contracts/src/register.sol
+++ contracts/src/register.sol
@@ -20,16 +20,16 @@ contract Register is Worldcoin {
         // checks if world ID is already registered
         require(!worldIDs[_worldID], "This World ID is already registered");
         worldIDs[_worldID] = true;
-        //TODO pass VotingPair
-        users[msg.sender] = User(id, _name, true, true, _worldID, 1, 1, 0, 0, new address[](0), new address[](0), 0);
+        // add user to user map
+        users[msg.sender] = User(id, _name, true, true, _worldID, 1, 1, 0, 0, new VotingPair[](0), new VotingPair[](0), 0);
         userAddress[id++] = msg.sender;
     }
     
     // Function to register an account as a Candidate
     function registerAsCandidate(bytes32 _name) external {
         require(!users[msg.sender].isRegistered, "User is already registered");
-        //TODO pass VotingPair
-        users[msg.sender] = User(id, _name, false, true, 0, 0, 0, 0, 2, new address[](0), new address[](0), 0);
+        // add user to user map
+        users[msg.sender] = User(id, _name, false, true, 0, 0, 0, 0, 2, new VotingPair[](0), new VotingPair[](0), 0);
         userAddress[id++] = msg.sender;
     }
 }
