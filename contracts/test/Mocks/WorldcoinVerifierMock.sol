584699a1b376e861e4e1f643295023ee45ef46bc
Sameer Kumar <sameer@nethermind.io>
2024-06-14 18:26:33 +0530

    removed internal fn from mock contract

--- contracts/test/Mocks/WorldcoinVerifierMock.sol
+++ contracts/test/Mocks/WorldcoinVerifierMock.sol
@@ -15,9 +15,5 @@ contract WorldcoinVerifierMock is IWorldcoinVerifier {
     )
         public
         override
-    { }
-
-    function hashToField(bytes memory value) internal pure returns (uint256) {
-        return uint256(keccak256(abi.encodePacked(value))) >> 8;
-    }
+    {}
 }
