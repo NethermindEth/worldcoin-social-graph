328fb48c471c0a3e2eebae5b6ed03234be86caac
Sameer Kumar <sameer@nethermind.io>
2024-06-17 14:27:30 +0530

    natspec: added comments for some variables

--- contracts/src/WorldcoinSocialGraphStorage.sol
+++ contracts/src/WorldcoinSocialGraphStorage.sol
@@ -13,20 +13,21 @@ contract WorldcoinSocialGraphStorage is IWorldcoinSocialGraphStorage{
     /// verified in that epoch
     mapping(address => mapping(uint256 => uint256) epochWeights) public userEpochWeights;
 
-    //x is the minimum power of Verified users needed in order to create fake Verified identities
+    /// @notice x is the minimum power of Verified users needed in order to create fake Verified identities
     uint16 internal constant x = 600;
-    // alpha parameter that determines the percentage of the voting power that will be returned to recommenders when a
+    /// @notice alpha parameter that determines the percentage of the voting power that will be returned to recommenders when a
     // candidate becomes verified
     uint8 internal constant a = 60;
-    // parameter that determines the rewards per epoch to be shared
+    /// @notice parameter that determines the rewards per epoch to be shared
     uint32 internal constant c = 140_000;
-    //stores candidates and world Id holders
+    // @notice stores candidates and world Id holders
     mapping(address => User) public users;
-    //sum of weights allocated to a candidate user
+    /// @notice sum of weights allocated to a candidate user
     mapping(address => uint256) public assignedWeight;
-
-    mapping(address => VotingPair[]) internal recommendees; // users who you vote/vouch for
-    mapping(address => VotingPair[]) internal recommenders; // users who vote/vouch for you
+    /// @notice users who you vote/vouch for
+    mapping(address => VotingPair[]) internal recommendees; 
+    /// @notice users who vote/vouch for you
+    mapping(address => VotingPair[]) internal recommenders;
 
     modifier onlyUnregistered(address _user) {
         require(users[_user].status == Status.UNREGISTERED, "WorldcoinGraph: ALREADY_REGISTERED");
