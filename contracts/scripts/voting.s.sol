9f345b32cb518a3a307e701c4955071db654859e
Sameer Kumar <sameer@nethermind.io>
2024-06-17 16:03:09 +0530

    removed deployment script temporarily

--- contracts/scripts/voting.s.sol
+++ contracts/scripts/voting.s.sol
@@ -2,15 +2,8 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
-import {Voting} from "../src/voting.sol";
 import {Script} from "../lib/forge-std/src/Script.sol";
 
 
 contract DeployVoting is Script {
-    function run() external returns (Voting) {
-        vm.startBroadcast();
-        Voting voting = new Voting();
-        vm.stopBroadcast();
-        return voting;
-    }
 }
